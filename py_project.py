bdc61b08bfea66fa5e8dff9d9c35dfcb6ddffbaa
AlecS <alec.salit@gmail.com>
2022-04-24 21:02:21 -0400

    creating bar graph

--- py_project.py
+++ py_project.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import pandas as pd
 from matplotlib import pyplot as plt
-from collections import Counter
 import seaborn as sns
 
 
@@ -10,7 +9,6 @@ df.info()
 # shows the release dates description stats
 df.describe().T.style.bar()
 
-df.drop_duplicates(inplace=True)
 # adding each column of the data's null values and showing percentages
 null_values = df.isnull().sum()
 null_values
@@ -18,15 +16,9 @@ total_null = null_values.sort_values(ascending=False)
 perc = (null_values / df.isnull().count()).sort_values(ascending=False)
 total = pd.concat([total_null, perc], axis=1, keys=['Total null values', 'Percentages of null values'])
 total.T.style.bar()
-#col = df._get_numeric_data().columns.tolist()
 
 # Getting rid of hashtags involved with titles
 df['Title'] = df['Title'].str.replace('#', '')
-
-# changing imdb scores to numeric 
-df['Imdb Score'] = df['Imdb Score'].str.replace('/10', '')
-df['Imdb Score'] = df['Imdb Score'].apply(pd.to_numeric)
-
 
 # showing the difference between Movies and TV shows 
 contentType_count = df['Content Type'].value_counts()
@@ -34,35 +26,103 @@ contentType_count.plot(kind='pie', legend=True, explode=(0, 0.1),
                         title = "Comparison of TV Shows vs Movies", colors=["blue", "yellow"],
                         figsize=(10,20))
 
+# changing imdb scores to numeric 
+df['Imdb Score'] = df['Imdb Score'].str.replace('/10', '')
+df['Imdb Score'] = df['Imdb Score'].apply(pd.to_numeric)
+df['Imdb Score']
+
+# create dataframe looking at mean of TV Shows vs Movies
+Content_imdb = pd.DataFrame(df.groupby('Content Type')['Imdb Score'].mean()).sort_values(ascending=False, by='Imdb Score')
+Content_imdb
+
+#Create a pie chart looking at 
+Content_imdb.plot(kind='pie', legend=True, explode=(0, 0.1), 
+                        title = "Comparison of TV Shows vs Movies", colors=["red", "yellow"],
+                        figsize=(10,20), subplots=True)
+
+
+# This chunk of code will apply a dataframe from the movie content type and split the genres
+# afterwards I stack the genres and use the mean imdb scores to plot this on a bar cahrt
+movie_genres_df = df[df['Content Type']== 'Movie']
+movie_genres_df = (movie_genres_df['Genres'].str.split(',', expand=True)
+            .stack()
+            .to_frame(name='Genres'))
+#movie_genres_df.index = movie_genres_df.index.droplevel(1)
+
+((movie_genres_df.join(df['Imdb Score']).groupby('Genres')
+       .mean())
+       .sort_values(by='Imdb Score')
+       .plot(kind='barh', figsize=(10,20)))
+
+
+
+
+
+tv_genres_df = df[df['Content Type']== 'TV Show']
+tv_genres_df = (tv_genres_df['Genres'].str.split(',', expand=True)
+            .stack()
+            .to_frame(name='Genre'))
+
+tv_genres_df.index = tv_genres_df.index.droplevel(1)
+tv_genres_df
+
+((tv_genres_df.join(df['Imdb Score'])
+       .groupby('Genre')
+       .mean())
+       .sort_values(by='Imdb Score')
+       .plot(kind='barh', figsize=(10,20)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+movie_ratings = df[df['Content Type'] == 'Movie'].groupby('Date added')['averageRating'].mean()
+
+
+
+
+
+
+
+
+
+movies_imdb = df.groupby(df['Content Type' == 'Movies'])
+movies_imdb = pd.DataFrame(movies_imdb)
+movies_imdb.describe()
+
 df.head()
-#
-
-genres_compare = df.groupby(["Imdb Score",'Genres'])["Genres"].count()
-#genres_compare.columns= ['Title']
-genres_compare
 
 
-genres = ", ".join(df['Genres']).split(", ")
-genres
-count_genres = Counter()
-for genre in genres:
-    count_genres[genre] += 1
-
-df.loc[df['language']].value_counts().plot(kind='bar')
-
-high_imdb_ratings = df.loc[df["Imdb Score"] >= '9.0/10', ["Title","Imdb Score","Content Type","count_genres" ]]
-high_imdb_ratings = high_imdb_ratings.sort_values(ascending=False, by='Imdb Score').reset_index(drop=True)
-high_imdb_ratings
 
 
-df_topGenres = pd.DataFrame (count_genres, columns = ['Genres','Genres Count'])
-df_topGenres.sort_values(ascending=True, by='Genres Count')
-df_topGenres
 
-#fig = plt.figure(figsize=(20, 4))
-#ax = plt.axes()
-#plt.title('Imdb Score')
-#sns.histplot(df['Imdb Score'], kde=True)
-#plt.xlabel('Imdb Score')
-#plt.ylabel('Duration')
-#plt.text(3,7, 'Skewness coeff. is:' + str(df['Imdb Score'].skew()))
+
+
+
+
+
+
+
+
+
+
